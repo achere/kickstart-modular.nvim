7c6e64f38b359c250511eabd3abcdbad537bbc7a
Alexander Chereshnev <chethelesser@gmail.com>
2024-05-25 13:18:52 +0200

    Enable some plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,13 +2,25 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
 return {
+  {
+    'mbbill/undotree',
+  },
   {
     'ray-x/lsp_signature.nvim',
     event = 'VeryLazy',
     opts = {},
     config = function(_, opts)
       require('lsp_signature').setup(opts)
+    end,
+  },
+  {
+    -- Theme inspired by Atom
+    'navarasu/onedark.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'onedark'
     end,
   },
 }
